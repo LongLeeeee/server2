6a330885f89eaee7f788966b1947b6bb60b7845d
Phát Lâm <22521068@gm.uit.edu.vn>
2024-06-13 20:33:25 +0700

    tp

---
+++ ServerProject/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace ServerProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_listen = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btn_listen
+            // 
+            this.btn_listen.Location = new System.Drawing.Point(478, 25);
+            this.btn_listen.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_listen.Name = "btn_listen";
+            this.btn_listen.Size = new System.Drawing.Size(80, 45);
+            this.btn_listen.TabIndex = 0;
+            this.btn_listen.Text = "Listen";
+            this.btn_listen.UseVisualStyleBackColor = true;
+            this.btn_listen.Click += new System.EventHandler(this.btn_listen_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(20, 25);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(454, 322);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btn_listen);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_listen;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ ServerProject/getData.cs
@@ -0,0 +1,158 @@
+﻿using FireSharp.Config;
+using FireSharp.Interfaces;
+using FireSharp.Response;
+using FireSharp.Extensions;
+using FireSharp.Exceptions;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+//using System.Windows.Media.Animation;
+using System.Net;
+using System.Windows.Forms;
+
+namespace ServerProject
+{
+    class Data
+    {
+        public string email { get; set; }
+        public string password { get; set; }
+        public string userName { get; set; }
+        public string name { get; set; }
+        public List<string> friends { get; set; }
+        public Notification notification { get; set; }
+    }
+    public class Notification
+    {
+        public string Sender { get; set; }
+        public string Receiver { get; set; }
+        public DateTime datetime { get; set; }
+        public string content { get; set; }
+    }
+    class tinNhan
+    {
+        public string sender { get; set; }
+        public string contentMess { get; set; }
+        public string receiver { get; set; }
+        public string roomkey {  get; set; }
+        public string groupName { get; set; }
+    }
+    class groupchat
+    {
+        public string[] receiver { get; set; }
+        public string roomkey { get; set; }
+        public string groupName { get; set; }
+
+    }
+    class Friend
+    {
+        public string username { get; set; }
+    }
+    class LoadInformation
+    {
+
+        public void getData(ref List<string> userList, IFirebaseClient client, ref Dictionary<string, string> check)
+        {
+            FirebaseResponse response = client.Get("users/");
+            if (response.Body != null)
+            {
+                var users = JsonConvert.DeserializeObject<Dictionary<string, Data>>(response.Body);
+                foreach (var item in users)
+                {
+                    if (item.Value.userName != null && item.Value != null && item.Key != null)
+                    {
+                        userList.Add(item.Value.userName);
+                    }
+                }
+            }
+            for (int j = 0; j < userList.Count(); j++)
+            {
+                if (userList[j] != null)
+                {
+                    FirebaseResponse response1 = client.Get("users/" + userList[j] + "/");
+                    Data data1 = response1.ResultAs<Data>();
+                    check.Add(data1.email, data1.password);
+                }
+            }
+        }
+    }
+    class CheckDataBase : LoadInformation
+    {
+        FirebaseConfig config = new FirebaseConfig()
+        {
+            AuthSecret = "JSe2prlcdWFSAjZFjeR3SSh4BFUnkbAbZ979GVx3",
+            BasePath = "https://testfirebase-c58f8-default-rtdb.firebaseio.com/"
+            //AuthSecret = "x8Z5vS17muGioNQZJgeGHU9V9nggI1dOcKDlzHmv",
+            //BasePath = "https://chat-application-of-team-12-default-rtdb.firebaseio.com/"
+        };
+        IFirebaseClient client;
+        public void CompareData(ref Dictionary<string, string> check, List<string> userList, Data loginUser, ref bool success, string code, ref string usernameOnline)
+        {
+            if (code == "Login")
+            {
+                for (int i = 0; i < userList.Count; i++)
+                {
+                    if ((loginUser.email.CompareTo(check.ElementAt(i).Key) == 0) && (loginUser.password.CompareTo(check.ElementAt(i).Value) == 0))
+                    {
+                        success = true;
+                        usernameOnline = userList[i];
+                        break;
+                    }
+                }
+            }
+            else if (code == "Register")
+            {
+                success = false;
+                for (int i = 0; i < userList.Count; i++)
+                {
+                    if ((loginUser.email.CompareTo(check.ElementAt(i).Key) == 0) || (loginUser.userName.CompareTo(userList[i]) == 0))
+                    {
+                        success = true;
+                        return;
+                    }
+                }
+                check.Add(loginUser.email, loginUser.password);
+                userList.Add(loginUser.userName);
+                usernameOnline = loginUser.userName;
+                var newUser = new Data
+                {
+                    userName = loginUser.userName,
+                    password = loginUser.password,
+                    email = loginUser.email,
+                    name = loginUser.userName,
+                };
+                client = new FireSharp.FirebaseClient(config);
+                SetResponse rs;
+                rs = client.Set<Data>($"users/{newUser.userName}", newUser);
+            }
+            else if (code == "ForgotPass")
+            {
+                for (int i = 0; i < userList.Count; i++)
+                {
+                    if ((loginUser.email.CompareTo(check.ElementAt(i).Key) == 0))
+                    {
+                        success = true;
+                        usernameOnline = userList[i];
+                        break;
+                    }
+                }
+            }
+            else if (code == "resetPass")
+            {
+                for (int i = 0; i < userList.Count; i++)
+                {
+                    if ((loginUser.email.CompareTo(check.ElementAt(i).Key) == 0))
+                    {
+                        success = true;
+
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
